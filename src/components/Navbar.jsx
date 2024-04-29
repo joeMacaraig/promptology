e53df7dc83f0c617e7ea60331e297df7d23832e3
Joseph Macaraig <jbmacaraig14@gmail.com>
2024-04-28 19:14:07 -0700

    uploaded new files
    connected google provider as well as created next-auth authentication, connected to mongodb

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,8 +7,8 @@ import Image from "next/image";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 export const Navbar = () => {
-  const isUserLoggedIn = true;
-  const [providers, setProviders] = useState(false);
+  const { data: session } = useSession();
+  const [providers, setProviders] = useState(null);
   const [drop, setDrop] = useState(null);
 
   useEffect(() => {
@@ -20,28 +20,43 @@ export const Navbar = () => {
   }, []);
 
   return (
-    <nav className="flex w-full mb-16 pt-3 justify-between px-4 items-center">
+    <nav className="flex w-full mb-16 pt-3 justify-between px-4 items-center mx-auto max-w-6xl">
       <Link href="/" className="flex gap-2 flex-center">
         <h1>Promptology</h1>
       </Link>
-      {/* Logged In */}
-      {isUserLoggedIn && (
-        <div className="gap-2 items-center sm:flex hidden">
-          <button className="border px-4 py-2 rounded-full border-black bg-black text-white">
-            Create Prompt
-          </button>
-          <button className="border px-4 py-2 rounded-full border-black">
-            Sign Out
-          </button>
-          <Link href="/profile">
-            <div className="p-5 bg-black rounded-full"></div>
-          </Link>
-        </div>
-      )}
+      {/* Desktop */}
+      <div className="sm:flex hidden">
+        {session?.user ? (
+          <div className="flex gap-4">
+            <Link href="/prompts">
+              <button className="border black-btn">Create Prompt</button>
+            </Link>
+            <button className="border px-4 py-2 rounded-full border-black" onClick={() => signOut()}>
+              Sign Out
+            </button>
+            <Link href="/profile">
+              <Image src={session?.user?.image} width={37} height={37} className="rounded-full" alt="profile-pic"/>
+            </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  className="black-btn"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
 
       {/* mobile */}
-      <div className="flex relative sm:hidden">
-        {isUserLoggedIn ? (
+      <div className="flex relative sm:hidden hover:cursor-pointer">
+        {session?.user ? (
           <div className="flex">
             <div
               className="w-[45px] h-[45px] bg-black rounded-full"
@@ -57,12 +72,34 @@ export const Navbar = () => {
                 <Link href="/prompts" onClick={() => setDrop(false)}>
                   <h1 className="hover:opacity-50">Create Prompts</h1>
                 </Link>
-                <button className="hover:opacity-50">Signout</button>
+                <button
+                  className="hover:opacity-50"
+                  onClick={() => {
+                    signOut();
+                    setDrop(false);
+                  }}
+                >
+                  Signout
+                </button>
               </div>
             )}
           </div>
         ) : (
-          <div></div>
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  className="black-btn"
+                  key={provider.name}
+                  onClick={() => {
+                    signOut();
+                    signIn(provider.id);
+                  }}
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
         )}
       </div>
     </nav>
--- src/components/PromptCard.jsx
+++ src/components/PromptCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const PromptCard = ({ username, email, prompt, image }) => {
   return (
-    <div className="border border-black rounded-lg p-4 w-full h-full flex flex-col gap-2">
+    <div className="border border-black bg-white rounded-lg p-4 w-full h-full flex flex-col gap-2">
       <div className="flex items-center gap-2">
         <div className="p-4 bg-black rounded-full"></div>
         <h1 className="font-medium">{username}</h1>
